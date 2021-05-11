b117d001cc8ffce2a8baab07097b1c2659c2da37
Mohammed Abdulaziz Alqadasi <55395496+mohammedAbdulziz@users.noreply.github.com>
2021-05-12 00:07:52 +0300

    Created the components

---
+++ src/components/Qualifications.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Qualifications() {
+    return (
+        <div>
+            <h2>hi  </h2>
+{/*This component contains the qualifications, Requirements, Experience, and Achievements*/}
+        </div>
+    )
+}
