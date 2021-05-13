3f4804845bedee97145732e4d8a838f0c9bb7f20
Mohammed Abdulaziz Alqadasi <55395496+mohammedAbdulziz@users.noreply.github.com>
2021-05-13 19:52:29 +0300

    Added Booking Comp

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ export default function app() {
       <Nav />
       <Hero />
       <Qualifications/>
+      <Booking />
       <Footer />
     </>
   )
--- src/components/Booking.jsx
+++ src/components/Booking.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import AppointmentLogo from "../Images/Svgs/appointmantIcon-01.svg";
 
 export default function Booking() {
     return (
-        <div>
-            {/* This contains the booking promotion */}
+            /* This contains the booking promotion */
+            <div className="booking flex">
+      <div className="bookingLeftSide">
+        <h1>
+          We believe that <span>every doctor</span> every where should be at the
+          top of their game.
+        </h1>
+        <h1>
+          <span>Book a time </span> right now.
+        </h1>
+      </div>
+      <div className="bookingRightSide">
+        <div className="heroButtons flex">
+          <button className="flex">
+            <img width="40" src={AppointmentLogo} alt="" /> Book a time
+          </button>
+          <small>20 available slots</small>
         </div>
+      </div>
+    </div>
+
     )
 }
